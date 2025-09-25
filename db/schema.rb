c2616d55754d34c698c8c91c4672c4584d2441dc
dcowett <david@davlinalli.com>
2025-09-25 05:56:07 -0400

    Generate Pins Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_23_120948) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_24_174903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

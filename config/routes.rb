a4b7b9e02bce2fb0b4941563e38e31a365557e48
dcowett <david@davlinalli.com>
2025-10-08 08:54:26 -0400

    fixed logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
+
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get "home/index"
 
   get "home/about"
